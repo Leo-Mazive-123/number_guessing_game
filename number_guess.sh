033a03c0bc7b903d641ae2602708bf778927d038
Leo-Mazive-123 <leomazive00@gmail.com>
2026-05-19 11:37:20 -0400

    fix: finalize output formatting for FCC

--- number_guess.sh
+++ number_guess.sh
@@ -47,17 +47,17 @@ do
   then
     echo "It's lower than that, guess again:"
   else
-    echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+  echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
 
-    NEW_GAMES=$((GAMES_PLAYED + 1))
+  NEW_GAMES=$((GAMES_PLAYED + 1))
 
-    if [[ -z $BEST_GAME || $BEST_GAME -eq 0 || $GUESS_COUNT -lt $BEST_GAME ]]
-    then
-      $PSQL "UPDATE users SET games_played=$NEW_GAMES, best_game=$GUESS_COUNT WHERE username='$USERNAME'"
-    else
-      $PSQL "UPDATE users SET games_played=$NEW_GAMES WHERE username='$USERNAME'"
-    fi
-
-    break
+  if [[ -z $BEST_GAME || $BEST_GAME -eq 0 || $GUESS_COUNT -lt $BEST_GAME ]]
+  then
+    $PSQL "UPDATE users SET games_played=$NEW_GAMES, best_game=$GUESS_COUNT WHERE username='$USERNAME'"
+  else
+    $PSQL "UPDATE users SET games_played=$NEW_GAMES WHERE username='$USERNAME'"
   fi
+
+  break
+fi
 done
