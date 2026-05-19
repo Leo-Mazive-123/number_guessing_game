a7e9eac72c598aaf0c639274478b41fc9851dca0
Leo-Mazive-123 <leomazive00@gmail.com>
2026-05-19 11:34:25 -0400

    fix: finalize number guessing game logic

--- number_guess.sh
+++ number_guess.sh
@@ -6,8 +6,6 @@ echo "Enter your username:"
 read USERNAME
 
 USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
-
-# trim whitespace
 USER_INFO=$(echo $USER_INFO | sed 's/^ *//;s/ *$//')
 
 if [[ -z $USER_INFO ]]
@@ -18,7 +16,6 @@ then
   BEST_GAME=""
 else
   IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
-
   GAMES_PLAYED=$(echo $GAMES_PLAYED | xargs)
   BEST_GAME=$(echo $BEST_GAME | xargs)
 
@@ -34,8 +31,8 @@ while true
 do
   read GUESS
 
-  # validate input
-  if ! [[ $GUESS =~ ^[0-9]+$ ]]
+  # MUST be integer check
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
   then
     echo "That is not an integer, guess again:"
     continue
@@ -45,16 +42,16 @@ do
 
   if [[ $GUESS -lt $SECRET_NUMBER ]]
   then
-    echo "Guess higher than that:"
+    echo "It's higher than that, guess again:"
   elif [[ $GUESS -gt $SECRET_NUMBER ]]
   then
-    echo "Guess lower than that:"
+    echo "It's lower than that, guess again:"
   else
     echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
 
     NEW_GAMES=$((GAMES_PLAYED + 1))
 
-    if [[ -z $BEST_GAME || $GUESS_COUNT -lt $BEST_GAME ]]
+    if [[ -z $BEST_GAME || $BEST_GAME -eq 0 || $GUESS_COUNT -lt $BEST_GAME ]]
     then
       $PSQL "UPDATE users SET games_played=$NEW_GAMES, best_game=$GUESS_COUNT WHERE username='$USERNAME'"
     else
